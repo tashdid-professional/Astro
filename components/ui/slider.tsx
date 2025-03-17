d36059db51900afff9d85f802bfc254f4aecaf12
tashdid-professional <tashdid.professional@gmail.com>
2025-03-18 04:20:26 +0600

    ar na vai dd

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 
 interface SliderProps
   extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {
